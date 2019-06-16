4cbfa736f35f35f921c611b9f8d6d7229df962bb
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-06-16 22:53:38 +0530

    Add files via upload

---
+++ detail.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+  welcome your name is :<?php echo $_POST["name"]; ?> <br>
+  email address is	   :<?php echo $_POST["email"];?> <br>
+  price                :<?php echo $_POST["price"];?> <br>
+  phone number         :<?php echo $_POST["phone"];?> <br>
+  address 			   :<?php echo $_POST["address"];?> <br>
+  housenumber          :<?php echo $_POST["postcode"];?> <br>
+  </body>
+  </html>
